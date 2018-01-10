82d5d3d08beccef121c179959bf27ff079c380f0
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2018-01-11 00:20:46 +0100

    automatic latest tag fetching with -V latest

--- colomoto-docker.py
+++ colomoto-docker.py
@@ -13,8 +13,8 @@ parser.add_argument("-w", "--workdir", default="/notebook", type=str,
     help="Workdir within the docker image")
 parser.add_argument("--shell", default=False, action="store_true",
     help="Start interactive shell instead of notebook service")
-parser.add_argument("-V", "--version", type=str, default="latest",
-    help="Version of docker image")
+parser.add_argument("-V", "--version", type=str, default="",
+    help="Version of docker image (latest to fetch the latest tag)")
 parser.add_argument("--port", default=8888, type=int,
     help="Local port")
 parser.add_argument("--image", default="colomoto/colomoto-docker",
@@ -24,13 +24,38 @@ parser.add_argument("--no-browser", default=False, action="store_true",
 parser.add_argument("docker_options", nargs="*")
 args = parser.parse_args()
 
+if args.version:
+    if args.version == "latest":
+        import json
+        try:
+            from urllib.request import urlopen
+        except ImportError:
+            from urllib2 import urlopen
+
+        print("# querying for latest tag of {}...".format(args.image))
+        url_api = "https://registry.hub.docker.com/v1/repositories/{}/tags".format(args.image)
+        tags = []
+        q = urlopen(url_api)
+        r = json.load(q)
+        q.close()
+        tags = [t["name"] for t in r if len(t["name"])==10 and "-" in t["name"]]
+        if not tags:
+            print("# ... none found! use 'latest'")
+            image_tag = "latest"
+        else:
+            image_tag = max(tags)
+            print("# ... using {}".format(image_tag))
+    else:
+        image_tag = args.version
+else:
+    image_tag = "latest"
 
 argv = ["docker", "run", "-it", "--rm", "-p", "%s:8888" % args.port]
 if args.bind:
     argv += ["--volume", "%s:%s" % (os.path.abspath(args.bind), args.workdir)]
 argv += ["-w", args.workdir]
 argv += args.docker_options
-argv += ["%s:%s" % (args.image, args.version)]
+argv += ["%s:%s" % (args.image, image_tag)]
 if args.shell:
     argv += ["bash"]
 

5c4869caed4acbf152c68acf3a3fa9ae2b5641da
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2018-02-07 15:50:56 +0100

    colomoto-docker: by default fetch the latest _persistent_ tag

--- colomoto-docker.py
+++ colomoto-docker.py
@@ -13,7 +13,7 @@ parser.add_argument("-w", "--workdir", default="/notebook", type=str,
     help="Workdir within the docker image")
 parser.add_argument("--shell", default=False, action="store_true",
     help="Start interactive shell instead of notebook service")
-parser.add_argument("-V", "--version", type=str, default="",
+parser.add_argument("-V", "--version", type=str, default="latest",
     help="Version of docker image (latest to fetch the latest tag)")
 parser.add_argument("--port", default=8888, type=int,
     help="Local port")
@@ -24,31 +24,28 @@ parser.add_argument("--no-browser", default=False, action="store_true",
 parser.add_argument("docker_options", nargs="*")
 args = parser.parse_args()
 
-if args.version:
-    if args.version == "latest":
-        import json
-        try:
-            from urllib.request import urlopen
-        except ImportError:
-            from urllib2 import urlopen
+if args.version == "latest":
+    import json
+    try:
+        from urllib.request import urlopen
+    except ImportError:
+        from urllib2 import urlopen
 
-        print("# querying for latest tag of {}...".format(args.image))
-        url_api = "https://registry.hub.docker.com/v1/repositories/{}/tags".format(args.image)
-        tags = []
-        q = urlopen(url_api)
-        r = json.load(q)
-        q.close()
-        tags = [t["name"] for t in r if len(t["name"])==10 and "-" in t["name"]]
-        if not tags:
-            print("# ... none found! use 'latest'")
-            image_tag = "latest"
-        else:
-            image_tag = max(tags)
-            print("# ... using {}".format(image_tag))
+    print("# querying for latest tag of {}...".format(args.image))
+    url_api = "https://registry.hub.docker.com/v1/repositories/{}/tags".format(args.image)
+    tags = []
+    q = urlopen(url_api)
+    r = json.load(q)
+    q.close()
+    tags = [t["name"] for t in r if len(t["name"])==10 and "-" in t["name"]]
+    if not tags:
+        print("# ... none found! use 'latest'")
+        image_tag = "latest"
     else:
-        image_tag = args.version
+        image_tag = max(tags)
+        print("# ... using {}".format(image_tag))
 else:
-    image_tag = "latest"
+    image_tag = args.version
 
 argv = ["docker", "run", "-it", "--rm", "-p", "%s:8888" % args.port]
 if args.bind:

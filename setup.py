d8bbfbe8de733d217ffd1f3ea6384d37570dc720
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2018-09-01 21:21:28 +0200

    colomoto-docker 6.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name="colomoto-docker",
-    version = "6.6",
+    version = "6.7",
     author = "Loïc Paulevé",
     author_email = "loic.pauleve@ens-cachan.org",
     url = "https://github.com/colomoto/colomoto-docker",

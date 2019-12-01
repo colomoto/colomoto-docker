ae1c6f04391c6c3405784505f58f5e82b4b5781f
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2019-12-01 14:28:23 +0100

    automatically create releases

---
+++ hooks/release_changes.py
@@ -0,0 +1,65 @@
+import json
+import re
+
+x = re.compile(r"^RUN\b((?:.+\\\n)*.+[^\\]$)", re.M)
+y = re.compile(r"(?<=conda\sinstall\b)([^&]+)", re.S|re.M)
+c = re.compile(r"-c\s([^\s]+)")
+w = re.compile(r"[^\\\s]+")
+
+def get_pkgs(lines, cfg):
+    pkgs = []
+    ignore_next = False
+    def handle_arg(m):
+        global ignore_next
+        arg = m.group(0)
+        if arg[0] == "-" or ignore_next or arg in ["nomkl"]:
+            pass
+        else:
+            pkg = arg.split("=")[:2]
+            pkgs.append(tuple(pkg))
+            ret = arg
+        if arg in ["-c"]:
+            ignore_next = True
+        else:
+            ignore_next = False
+    def handle_install(lines):
+        w.sub(handle_arg, lines.group(1))
+    def handle_run(m):
+        lines = m.group(0)
+        y.sub(handle_install, m.group(1))
+    x.sub(handle_run, lines)
+    return pkgs
+
+if __name__ == "__main__":
+    inpfile = "Dockerfile"
+    cfgfile = __file__.replace(".py", ".json")
+    with open(cfgfile) as fp:
+        cfg = json.load(fp)
+    with open(inpfile) as f:
+        data = f.read()
+    pkgs = get_pkgs(data, cfg)
+    print("""Fetch and run the latest image with
+```
+colomoto-docker -V latest
+```
+
+## Packages
+
+|Package|Description|Version|
+| --- | --- | --- |""")
+    for pkg, version in pkgs:
+        if pkg not in cfg:
+            continue
+        pkg_cfg = cfg[pkg]
+        name = pkg_cfg.get("name",pkg)
+        release_url = pkg_cfg.get("release_url","").format(version)
+        if release_url:
+            version = "[{version}]({release_url})".format(
+                version=version,
+                release_url=release_url)
+        desc = pkg_cfg.get("description","N/A")
+        print("| {name} | {description} | {version}|".format(
+            name=name,
+            description=desc,
+            version=version))
+

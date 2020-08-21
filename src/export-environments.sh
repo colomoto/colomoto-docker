e5ec9347b4b58c3d23386c4b93e6ecfb45a7de02
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2020-08-21 23:35:52 +0200

    use explicit listing of installed conda packages

--- src/export-environments.sh
+++ src/export-environments.sh
@@ -4,6 +4,6 @@ IMAGE=colomoto/colomoto-docker:$TAG
 mkdir -p $TAG && cd $TAG
 docker pull ${IMAGE}
 docker run --rm ${IMAGE} apt list --installed > debian-installed-${TAG}.txt
-docker run --rm ${IMAGE} conda env export > conda-environment-${TAG}-strict.yml
+docker run --rm ${IMAGE} conda list --explicit --md5 > conda-environment-${TAG}-explicit.yml
 docker run --rm ${IMAGE} conda env export -c conda-forge -c colomoto --from-history > conda-environment-${TAG}.yml||true
 docker rmi ${IMAGE}
